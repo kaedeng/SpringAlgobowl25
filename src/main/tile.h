8bb44c28d01e55ba6a1e5d4510d012626e8ed6e8
Swiffels <54492510+Swiffels@users.noreply.github.com>
2025-02-18 11:41:29 -0700

    Create tile class (#1)
    * Create tile class
    
    * Update tile class

---
+++ src/main/tile.h
@@ -0,0 +1,28 @@
+#pragma once
+
+class Tile{
+    private:
+    
+    Type type;
+    char tentDir = 'X';
+        
+    public:
+
+        Tile(Type tileType){
+            type = tileType;
+        }    
+
+        Type getType(){ return type; };
+        void setType(Type type){ this->type = type; };
+
+        char getDir(){ return tentDir; };
+        void setDir(char tentDir){ this->tentDir = tentDir; }
+
+};
+
+enum class Type {
+    TREE,
+    TENT,
+    NONE
+};
+
