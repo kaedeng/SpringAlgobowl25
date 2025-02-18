9f394dd777ff5e2cadaf62c00c04374383b0c48f
Swiffels <swp9gaming@gmail.com>
2025-02-18 15:35:46 -0700

    Fix tests cmake issue

--- src/test/hello_test.cc
+++ src/test/hello_test.cc
@@ -17,9 +17,9 @@ TEST(BasicTest, BoardUnitTests){
   std::vector<size_t> colTentNum = {4, 3, 2, 1};
 
   std::vector<std::vector<Tile>> boardMap = {
-    {Tile(Type::NONE),Tile(Type::NONE),Tile(Type::NONE),Tile(Type::TREE)},
-    {Tile(Type::NONE),Tile(Type::TREE),Tile(Type::NONE),Tile(Type::NONE)},
-    {Tile(Type::NONE),Tile(Type::NONE),Tile(Type::NONE),Tile(Type::NONE)},
+    {Tile(Type::NONE, 0, 0),Tile(Type::NONE, 0, 1),Tile(Type::NONE, 0, 2),Tile(Type::TREE, 0, 3)},
+    {Tile(Type::NONE, 1, 0),Tile(Type::TREE, 1, 1),Tile(Type::NONE, 1, 2),Tile(Type::NONE, 1, 3)},
+    {Tile(Type::NONE, 2, 0),Tile(Type::NONE, 2, 1),Tile(Type::NONE, 2, 2),Tile(Type::NONE, 2, 3)},
   };
 
   Board board = Board(3, 4, rowTentNum, colTentNum, boardMap);
