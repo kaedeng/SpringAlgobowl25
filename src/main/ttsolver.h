79771fe1cbadec87506e839e75fb6277b52be6ff
Swiffels <swp9gaming@gmail.com>
2025-02-22 03:32:12 -0700

    IT WORKS

--- src/main/ttsolver.h
+++ src/main/ttsolver.h
@@ -1,5 +1,10 @@
 #pragma once
+
+#include "board.h"
+
 #include <stdlib.h>
+#include <vector>
+#include <random>
 
 /**
  * @brief Our main algorithm for solving the Tents and Trees problem
@@ -13,7 +18,24 @@ class TTSolver {
      * @brief Construct a new TTSolver object
      * Should just pass the information from parsing files to initialize this
      */
-    TTSolver();
+    TTSolver(size_t generationSize, size_t maxGenerations, size_t sameChildrenNum, const Board& board) : gen(std::random_device{}()), generationSize(generationSize), maxGenerations(maxGenerations), sameChildrenNum(sameChildrenNum), startingBoard(board) {};
+
+    void solve();
+
+    private:
+
+    std::mt19937 gen;
+
+    size_t generationSize;
+    size_t maxGenerations;
+    size_t sameChildrenNum;
+    Board startingBoard;
+
+    size_t numTiles;
+    size_t numRows;
+    size_t numCols;
+
+    std::vector<Board> currentGeneration{generationSize, startingBoard};
 
     /**
      * @brief Creates the output file with the current iteration of the chart.
@@ -23,6 +45,30 @@ class TTSolver {
      */
     bool createOutput();
 
-    private:
+    /**
+     * @brief Creates initial boards based on generationSize.
+     */
+    void initialize(const Board&);
+
+    /**
+     * @brief Main iteration loop
+     */
+    void iterate();
+
+    /**
+     * @brief Selects the next generation
+     */
+    void selection();
+
+    /**
+     * @brief Creates the next generation and mixes genes
+     */
+    void crossover();
+
+    /**
+     * @brief Mutates the next generation
+     */
+    void mutation();
+
 
 };
